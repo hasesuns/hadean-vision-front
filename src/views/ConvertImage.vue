c0686c6290d0a5d1e7a516d7acaa0fd2a79984e6
hasegawa akiyuki <hase.eeicm@gmail.com>
2021-09-04 19:28:46 +0900

    :lipstick: Add file form

--- src/views/ConvertImage.vue
+++ src/views/ConvertImage.vue
@@ -2,5 +2,40 @@
   <div class="convert">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <form class="form" @submit.prevent="submitImg">
+      <input type="file" accept="img/*" @change="changeImg" />
+      <button type="submit" class="button">click</button>
+    </form>
+    <div>
+      <img :src="postData.thumbnail" alt="" />
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      thumbnail: "",
+      postData: {
+        thumbnail: "",
+      },
+    };
+  },
+  methods: {
+    changeImg(e) {
+      this.thumbnail = e.target.files[0];
+      if (this.thumbnail) {
+        const reader = new FileReader();
+        reader.onload = () => {
+          this.postData.thumbnail = reader.result + "";
+        };
+        reader.readAsDataURL(this.thumbnail);
+        console.log("Input image is prepared");
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
