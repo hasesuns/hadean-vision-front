f7ae6724afa66974a34c2a7a55bd3fd6921031b9
hasegawa akiyuki <hase.eeicm@gmail.com>
2021-09-12 14:32:42 +0900

    :tada: Add post convertAPI function with axios

--- src/views/ConvertImage.vue
+++ src/views/ConvertImage.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="convert">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <FileForm v-model="previewImage" @imageChange="onImageChange" />
-    <input @click="postImage" type="button" value="POST" />
-    <img :src="previewImage" />
+    <FileForm @imageSelect="onImageSelect" />
+    <button id="convert_button" @click="postImage" type="submit" value="POST">
+      Convert
+    </button>
+    <div id="input_image">
+      <img :src="inputImageBlobURL" />
+    </div>
     <div>
-      {{ previewImage }}
+      {{ inputImageBlobURL }}
+    </div>
+    <div id="output_image">
+      <img :src="outputImageBlobURL" />
     </div>
   </div>
 </template>
@@ -13,7 +20,7 @@
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import FileForm from "@/components/FileForm.vue"; // @ is an alias to /src
-import axios from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 export default defineComponent({
   name: "ConvertImage",
@@ -21,17 +28,44 @@ export default defineComponent({
     FileForm,
   },
   setup() {
-    let previewImage = ref("");
-    const onImageChange = (selectedImage: string) => {
-      previewImage.value = selectedImage;
+    let inputImageBlobURL = ref("");
+    let inputImageName = "";
+    let outputImageBlobURL = ref("");
+
+    const onImageSelect = (selectedImage: File) => {
+      inputImageBlobURL.value = window.URL.createObjectURL(selectedImage);
+      inputImageName = selectedImage.name;
     };
-    const postImage = () => {
+    const postImage = async () => {
       console.log("post");
+      const url = "http://127.0.0.1:8000/convert/";
+
+      const formData = new FormData();
+      const blob = await fetch(inputImageBlobURL.value).then((r) => r.blob());
+      const apiInputValName = "file";
+      formData.append(apiInputValName, blob, inputImageName);
+
+      const config: AxiosRequestConfig = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        responseType: "blob",
+      };
+
+      await axios
+        .post(url, formData, config)
+        .then(function (response: AxiosResponse) {
+          outputImageBlobURL.value = window.URL.createObjectURL(response.data);
+        })
+        .catch(function (error: Error) {
+          console.log(error);
+        });
     };
     return {
-      previewImage,
-      onImageChange,
+      inputImageBlobURL,
+      onImageSelect,
       postImage,
+      outputImageBlobURL,
     };
   },
 });
