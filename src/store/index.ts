380153254ec80161b52a1fc62a03cebba642c0ed
hasegawa akiyuki <hase.eeicm@gmail.com>
2022-02-21 10:49:14 +0900

    :construction: Create postApi action

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,28 @@
+import ConvertImageInputData from "@/usecases/convertImage/ConvertImageInputData";
+import axios, { AxiosRequestConfig } from "axios";
 import { createStore } from "vuex";
 
 export default createStore({
   state: {},
   mutations: {},
-  actions: {},
+  actions: {
+    async postApi(_, payload: ConvertImageInputData) {
+      console.log("post");
+      const url = process.env.VUE_APP_API_BASE + "/convert/";
+      const formData = new FormData();
+      const blob = await fetch(payload.inputImageBlobURL).then((r) => r.blob());
+      const apiInputValName = "file";
+      formData.append(apiInputValName, blob, payload.inputFileName);
+
+      const config: AxiosRequestConfig = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        responseType: "blob",
+      };
+
+      return axios.post(url, formData, config);
+    },
+  },
   modules: {},
 });
